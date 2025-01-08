a102a1fb11586b3ffa270eb1e34a4c05a5a7c580
cmrobbin <113061287+cmrobbin@users.noreply.github.com>
2025-01-08 15:17:30 -0500

    Update circ_demo.m
    Fixed issue with wrong input to imagesc_mask

--- mfiles/Colormaps/circular_colormaps/circ_demo.m
+++ mfiles/Colormaps/circular_colormaps/circ_demo.m
@@ -18,14 +18,14 @@ angle(X<0)=angle(X<0)+pi;
 angle(angle<0)=angle(angle<0)+2*pi;
 
 figure('position',[w0+w/10,h0+h/4,w/3,h/2])
-imagesc_mask(angle,twilight,[0 2*pi],0,[],mask)
+imagesc_mask(angle,twilight,[0 2*pi],0,mask)
 set(gca,'ydir','normal')
 labels={'0','\pi/2','\pi','3\pi/2','2\pi'};
 colorbar('xtick',[0 0.5 1 1.5 2]*pi,'xticklabel',labels)
 title('"twilight"')
 
 figure('position',[w0+w*(5/10),h0+h/4,w/3,h/2])
-imagesc_mask(angle,twilight_shifted,[0 2*pi],0,[],mask)
+imagesc_mask(angle,twilight_shifted,[0 2*pi],0,mask)
 set(gca,'ydir','normal')
 labels={'0','\pi/2','\pi','3\pi/2','2\pi'};
 colorbar('xtick',[0 0.5 1 1.5 2]*pi,'xticklabel',labels)
